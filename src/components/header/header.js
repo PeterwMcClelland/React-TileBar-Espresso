442a86df1a82254f7e482519e8826a7e53d24b9a
Peter McClelland <peterwmcclelland@gmail.com>
2023-01-04 16:58:56 -0600

    navbar

---
+++ src/components/header/header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+function Header() {
+    return (
+        <header className="header">
+            <h1 className="titleh1">TileBar</h1>
+            <nav className="navbar">
+                <ul className="navcomponents">
+                    <li>About</li>
+                    <li>Gallery</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
