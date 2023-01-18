3ae4d72547304fe421110b66cadf2cf64df47e48
Peter McClelland <peterwmcclelland@gmail.com>
2023-01-17 20:16:09 -0600

    new gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -9,8 +9,20 @@ function Gallery()  {
       <body>
   <div class="gallery">
 
-    <img alt="coffee" src="./public/assets/img/IMG_0426.jpeg"></img>
-    <img alt="coffee" src="./public/assets/img/IMG_7672.jpeg"></img>
+    <img alt="coffee" src={require("../assets/img/P1220636.jpg")}></img>
+    <img alt="coffee" src={require("../assets/img/IMG_0426.jpeg")}></img>
+    <img alt="coffee" src={require("../assets/img/P1220535.JPG")}></img>
+    <img alt="coffee" src={require("../assets/img/IMG_7672.JPG")}></img>
+    <img alt="coffee" src={require("../assets/img/P1220652.jpg")}></img>
+    <img alt="coffee" src={require("../assets/img/IMG_0691.jpeg")}></img>
+    <img alt="coffee" src={require("../assets/img/P1230014.jpg")}></img>
+    <img alt="coffee" src={require("../assets/img/mainimg.JPG")}></img>
+    <img alt="coffee" src={require("../assets/img/P1220576.jpg")}></img>
+    <img alt="coffee" src={require("../assets/img/IMG_0143.JPG")}></img>
+    <img alt="coffee" src={require("../assets/img/IMG_1417.JPG")}></img>
+    <img alt="coffee" src={require("../assets/img/IMG_1428.JPG")}></img>
+    <img alt="coffee" src={require("../assets/img/IMG_7581.JPG")}></img>
+    <img alt="coffee" src={require("../assets/img/IMG_1418.JPG")}></img>
 
   </div>
   </body>
