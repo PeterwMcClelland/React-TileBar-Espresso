93801ba78e51ebcebee63c98f299648f11a5a2f6
Peter McClelland <peterwmcclelland@gmail.com>
2023-01-22 18:57:43 -0600

    about css

--- src/components/About.js
+++ src/components/About.js
@@ -8,12 +8,17 @@ function About()  {
         <>
        <body className="aboutbody">
            <section class='abouticons'>
-       <div>
+       <div className="col2">
+       <div className="col1">
+          TileBar Espresso is an espresso catering service started in Portland, OR in 2016. Our goal is to provide professional 
+          grade espresso to your event or business. Our stylish, compact espresso cart makes it easy to set up quickly and start serving. 
+          Meeting your needs is our top priority. We will work with you to provide the best espresso experience for your event.
+          </div>
         <ul className="flex-row-about">
     
             <a className="iconstyle" href="https://www.facebook.com/tilebarespresso/" target='blank'>
                 <lu>
-                <i class="fa-brands fa-square-facebook"></i>
+                <i class="fa-brands fa-square-facebook fa-2xl"></i>
               </lu>
             </a>
             
